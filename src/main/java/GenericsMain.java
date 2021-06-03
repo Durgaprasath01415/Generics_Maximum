2f06eb763ae0caecebbcf211be3204ace52a88e6
Durgaprasath01415 <83449179+Durgaprasath01415@users.noreply.github.com>
2021-06-03 22:44:52 +0530

    ADDED: Compare of Integer number for  maximum number

--- src/main/java/GenericsMain.java
+++ src/main/java/GenericsMain.java
@@ -1,3 +1,10 @@
 public class GenericsMain {
-
+    public static Integer compareIntegerMaximum(Integer firstNumber, Integer secondNumber, Integer thirdNumber){
+        Integer maximumNumber = firstNumber;
+        if(secondNumber.compareTo(maximumNumber)>0)
+            maximumNumber = secondNumber;
+        if(thirdNumber.compareTo(maximumNumber)>0)
+            maximumNumber = thirdNumber;
+        return maximumNumber;
+    }
 }
--- src/test/java/GenericsTest.java
+++ src/test/java/GenericsTest.java
@@ -2,5 +2,20 @@ import org.junit.Assert;
 import org.junit.Test;
 
 public class GenericsTest {
-
+    GenericsMain genericsMain = new GenericsMain();
+    @Test
+    public void givenFirstNumber_whenFirstNumberMaximum_shouldReturnMaximum(){
+        Integer max = GenericsMain.compareIntegerMaximum(6,4,2);
+        Assert.assertEquals((Integer)6,max);
+    }
+    @Test
+    public void givenFirstNumber_whenSecondNumberMaximum_shouldReturnMaximum(){
+    Integer max = GenericsMain.compareIntegerMaximum(4,6,2);
+        Assert.assertEquals((Integer)6,max);
+    }
+    @Test
+    public void givenFirstNumber_whenThirdNumberMaximum_shouldReturnMaximum(){
+        Integer max = GenericsMain.compareIntegerMaximum(2,4,6);
+        Assert.assertEquals((Integer)6,max);
+    }
 }
